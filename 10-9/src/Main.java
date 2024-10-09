8bf361e12be34cc8b78902c39bf8a9eaf2729292
MrSodiq <136502782+Sodiqjon1@users.noreply.github.com>
2024-10-09 21:34:46 +0500

    Add files via upload

---
+++ 10-9/src/Main.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// ctrl + D dublikat
+// alt + J
+public class Main {
+    public static void main(String[] args) {
+
+////        text
+//        String alfa="Alfa";
+//        String beta="Beta";
+//        System.out.println(alfa+" "+beta);
+
+////        lenght metod
+//        String gama="I'm Pain";
+//        System.out.println(gama.length()+" ta");
+
+////        equals metodi
+//        String delta="Minecraft";
+//        String epsilond="Hoi4";
+//        System.out.println(delta.equals(epsilond));
+
+////        contains metod
+//        String zeta="How I ?";
+//        System.out.println(zeta.contains("I"));
+
+////        startsWith() metod
+//        String eta="I don't know me !";
+//        System.out.println(eta.startsWith("me"));
+
+////        endsWith() metod
+//        String teta="What is my name ?";
+//        System.out.println(teta.endsWith("!"));
+
+////        int butunsonlar
+//        int yota=454;
+//        int kappa=404;
+//        System.out.println(yota+kappa);
+//        System.out.println(yota-kappa);
+//        System.out.println(yota*kappa);
+//        System.out.println(yota/kappa);
+
+////        max and mine value
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+
+////        double toliqson (davirni butulamidi)
+//        double lamda=707;
+//        Double myu=505.0;
+//        System.out.println(lamda+myu);
+
+////        boolean true or false
+//        boolean nyu=false;
+//        boolean ksi=true;
+//        System.out.println(nyu);
+//        System.out.println(ksi);
+
+////        float (davrni butunlavoradi)
+//        float omikron=404;
+//        System.out.println(omikron/4);
+
+////        Scanner
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("Birichi");
+//        int pi= scanner.nextInt();
+//        System.out.println("Ikkichi");
+//        int ro= scanner.nextInt();
+//        System.out.println(pi+ro);
+//
+//        System.out.println("Birichi");
+//        String sigma= scanner.next();
+//        System.out.println("Ikkichi");
+//        String tau= scanner.next();
+//        System.out.println(sigma+" "+tau);
+
+////            next() nextLine()
+////            next() probelga cha chiqaradi
+//        System.out.println("Birichi");
+//        Double ipsilon= scanner.nextDouble();
+//        System.out.println("Ikkichi");
+//        Double fi= scanner.nextDouble();
+//        System.out.println(ipsilon+fi);
+
+////        if
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("yoshni yoz: ");
+//        int hi=  scanner.nextInt();
+//        if (hi == 16) {
+//            System.out.println("Ma pasprtini kema qaytib");
+//        } if ( hi > 16) {
+//            System.out.println("Qarib ketibsan");
+//        } else{
+//            System.out.println("Ture osvol");
+//        }
+
+//        math funksiy
+
+//        1metod sprt kvadrat ildiz
+        System.out.println(Math.sqrt(144));
+
+//        2metod darajaga kotaradi pow
+        System.out.println(Math.pow(12,2));
+
+//        3metod syntax
+        
+    }
+}
---
+++ 10-9/src/test.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class test{
+    public static void main(String[] args) {
+////        test alfa
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("yoshni yoz: ");
+//        int alfa=  scanner.nextInt();
+//        if (alfa >= 7 && alfa <=18) {
+//            System.out.println("Oqisen boladi");
+//        } else {
+//            System.out.println("Mk ga kiromisan");
+//        }
+
+////        beta
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("Boy: ");
+//        int beta=  scanner.nextInt();
+//        System.out.println("ves: ");
+//        int gama=  scanner.nextInt();
+//        System.out.println("pul: ");
+//        int delta= scanner.nextInt();
+//        if (beta >= 180 && gama>=70 && gama<90 && delta==3000) {
+//            System.out.println("Ishka olindin.");
+//            System.out.println("Vashe zvaniya:Redavoy");
+//        } if (delta>3000) {
+//            System.out.println("Assalomu alaykum orto general.");
+//            System.out.println("Vashe zvaniya:General");
+//
+//        } else {
+//            System.out.println("Boshqa ish qidir.");
+//            System.out.println("'Nishi'");
+//        }
+
+//        gama
+//        (% qoldiq topadi)
+        int a=5;
+        int b=6;
+        int c=9;
+        int d=10;
+        int f=11;
+        System.out.println(a*b-c/d+a-f);
+    }
+}
+

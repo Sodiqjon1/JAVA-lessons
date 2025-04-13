5fe800964d0c4ec7583481cf710a3815d57dba41
MrSodiq <136502782+Sodiqjon1@users.noreply.github.com>
2025-04-13 23:04:55 +0500

    Add files via upload

---
+++ task/src/Main.java
@@ -0,0 +1,96 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+////        1
+//        int a ;
+//
+//        System.out.println("Son : ");
+//        a = scanner.nextInt();
+//
+//        second.fun0(a);
+
+////        2
+//        int a , b ;
+//
+//        System.out.println("a son : ");
+//        a = scanner.nextInt();
+//        System.out.println("b son : ");
+//        b = scanner.nextInt();
+//
+//        System.out.println(second.fun1(a , b));
+
+////        3
+//        second.fun2();
+
+////        4
+//        int a ;
+//
+//        System.out.println("Son : ");
+//        a = scanner.nextInt();
+//
+//        System.out.println(second.fun3(a));
+
+////        5
+//        int a , b ;
+//
+//        System.out.println("a son: ");
+//        a = scanner.nextInt();
+//        System.out.println("b son: ");
+//        b = scanner.nextInt();
+//
+//        System.out.println(second.fun4( a , b ));
+
+////        6
+//        int n , b = 0;
+//
+//        n = scanner.nextInt();
+//
+//        int a [] = new int[n];
+//
+//        for (int i = 0; i < n ; i++) {
+//            a[i] = scanner.nextInt();
+//        }
+//
+//        System.out.println(second.fun5( a , n , b));
+
+////        7
+//        int a , b = 0 ;
+//
+//        a = scanner.nextInt();
+//
+//        System.out.println(second.fun6( a , b ));
+
+////        8
+//        System.out.println("Text");
+//        String a = scanner.next();
+//
+//        int c = 0, n = a.length();
+//
+//        String g = scanner.next();
+//
+//        char q = g.charAt(0);
+//        char b [] = new char[n];
+//
+//        for (int i = 0; i < n ; i++) {
+//            b[i] = a.charAt(i);
+//        }
+//
+//        System.out.println(second.fun7(b,n,c,q));
+
+////        9
+//        System.out.println("Yoshini yoz: ");
+//        int a = scanner.nextInt();
+//
+//        second.fun8(a);
+
+////        10
+//        int a , b = 0;
+//        System.out.println("F da harorat : ");
+//        a = scanner.nextInt();
+//
+//        System.out.println(second.fun9( a , b ));
+    }
+}
---
+++ task/src/second.java
@@ -0,0 +1,78 @@
+import java.util.Objects;
+
+public class second {
+
+////    1
+//    public static void fun0(int a ) {
+//        if (a % 2 == 0){
+//            System.out.println("Juft");
+//        }
+//        if (a % 2 != 0){
+//            System.out.println("Toq");
+//        }
+//    }
+
+////    2
+//    public static int fun1(int a , int b ) {
+//        int sum = a + b;
+//        return sum;
+//    }
+
+////    3
+//    public static void fun2(){
+//        System.out.println("Guden dag");
+//    }
+
+////    4
+//    public static int fun3(int a) {
+//        return a * a;
+//    }
+
+////    5
+//    public static  int fun4(int a ,int b ){
+//        if (a > b){
+//            return b;
+//        }else{
+//            return a;
+//        }
+//    }
+
+////    6
+//    public static int fun5(int a[] , int n , int b) {
+//        for (int i = 0; i < n ; i++) {
+//            b = b + a[i];
+//        }
+//        return b;
+//    }
+
+////    7
+//    public  static int fun6(int a ,int b){
+//        b = a * -1;
+//        return b;
+//    }
+
+////    8
+//    public static int  fun7(char b[] ,int n , int c , char q){
+//        for (int i = 0; i < n ; i++) {
+//            if (Objects.equals(b[i], q)){
+//                c++;
+//            }
+//        }
+//        return c;
+//    }
+
+////    9
+//        public static void fun8(int a){
+//            if(60<=a){
+//                System.out.println("Pensiayner");
+//            } else {
+//                System.out.println("Ishla hali");
+//            }
+//        }
+
+////    10
+//    public  static  int fun9(int a,int b){
+//        b = a / 4;
+//        return b;
+//    }
+}
